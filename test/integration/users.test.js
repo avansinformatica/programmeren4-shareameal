1fabf4d4ff5402bc7bba1a55c8c9ef58e79d6ca7
Robin Schellius <r.schellius@avans.nl>
2023-04-20 12:30:38 +0200

    UserRegistration test toegevoegd

--- test/integration/users.test.js
+++ test/integration/users.test.js
@@ -7,6 +7,15 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('UC-201 Registreren als nieuwe user', () => {
+  it('TC-201-1 - Verplicht veld ontbreekt', (done) => {
+    // Testen die te maken hebben met authenticatie of het valideren van
+    // verplichte velden kun je nog niet uitvoeren. Voor het eerste inlevermoment
+    // mag je die overslaan.
+    // In een volgende huiswerk opdracht ga je deze tests wel uitwerken.
+    // Voor nu:
+    done();
+  });
+
   it('TC-201-5 - User succesvol geregistreerd', (done) => {
     // nieuwe user waarmee we testen
     const newUser = {
@@ -16,6 +25,64 @@ describe('UC-201 Registreren als nieuwe user', () => {
       email: 'hvd@server.nl'
     };
 
+    // Voer de test uit
+    chai
+      .request(server)
+      .post('/api/register')
+      .send(newUser)
+      .end((err, res) => {
+        assert(err === null);
+
+        res.body.should.be.an('object');
+        let { data, message, status } = res.body;
+
+        status.should.equal(200);
+        message.should.be.a('string').equal('User successfully registered');
+        data.should.be.an('object');
+
+        // OPDRACHT!
+        // Bekijk zelf de API reference op https://www.chaijs.com/api/bdd/
+        // Daar zie je welke chained functions je nog meer kunt gebruiken.
+        data.should.include({ id: 2 });
+        data.should.not.include({ id: 0 });
+        data.id.should.equal(2);
+        data.firstname.should.equal('Hendrik');
+
+        done();
+      });
+  });
+});
+
+describe('UC-202 Opvragen van overzicht van users', () => {
+  it('TC-202-1 - Toon alle gebruikers, minimaal 2', (done) => {
+    // Voer de test uit
+    chai
+      .request(server)
+      .post('/api/register')
+      .send(newUser)
+      .end((err, res) => {
+        assert(err === null);
+
+        res.body.should.be.an('object');
+        let { data, message, status } = res.body;
+
+        status.should.equal(200);
+        message.should.be.a('string').equal('User successfully registered');
+        data.should.be.an('object');
+
+        // OPDRACHT!
+        // Bekijk zelf de API reference op https://www.chaijs.com/api/bdd/
+        // Daar zie je welke chained functions je nog meer kunt gebruiken.
+        data.should.include({ id: 2 });
+        data.should.not.include({ id: 0 });
+        data.id.should.equal(2);
+        data.firstname.should.equal('Hendrik');
+
+        done();
+      });
+  });
+
+  it('TC-202-2 - Toon gebruikers met zoekterm op niet-bestaande velden', (done) => {
     // Voer de test uit
     chai
       .request(server)
